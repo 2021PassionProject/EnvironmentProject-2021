c5ac1b50a754af02b5c9e1d6519ad5360870a9fa
Intrager <intrager@naver.com>
2021-07-03 23:45:19 +0900

    Add hellWorld[mul]

--- hellWorld.js
+++ hellWorld.js
@@ -5,5 +5,5 @@ document.write("EnvironmentProject"+<br>);
 var passion = 2021;
 var project = 7;
 
-var environment = passion + project;
+var environment = passion * project;
 document.write(environment);
