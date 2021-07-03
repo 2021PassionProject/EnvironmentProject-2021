ebd4ef851368915cf175244a9276a1bf12c4797d
Intrager <intrager@naver.com>
2021-07-03 23:35:16 +0900

    Add hellWorld [add]

--- hellWorld.js
+++ hellWorld.js
@@ -1,3 +1,9 @@
 Console.log("passionProject");
 
 document.write("EnvironmentProject"+<br>);
+
+var passion = 2021;
+var project = 7;
+
+var environment = passion + project;
+document.write(environment);
