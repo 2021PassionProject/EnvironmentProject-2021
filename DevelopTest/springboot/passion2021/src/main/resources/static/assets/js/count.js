0223174af1dd3a99833a60902055605c0a47dfd7
jeonminseo <mamami0201@naver.com>
2021-09-20 18:27:29 +0900

    Refresh 카운트

---
+++ DevelopTest/springboot/passion2021/src/main/resources/static/assets/js/count.js
@@ -0,0 +1,17 @@
+
+    expireDate = new Date
+    expireDate.setMonth(expireDate.getMonth() + 6)
+    hitCt = eval(cookieVal("pageHit"))
+    hitCt++
+    document.cookie = "pageHit=" + hitCt + ";expires=" + expireDate.toGMTString()
+    function cookieVal(cookieName) {
+        thisCookie = document.cookie.split("; ")
+        for (i = 0; i < thisCookie.length; i++) {
+            if (cookieName == thisCookie[i].split("=")[0]) {
+
+                return thisCookie[i].split("=")[1]
+            }
+
+    }
+        return 0
+}
