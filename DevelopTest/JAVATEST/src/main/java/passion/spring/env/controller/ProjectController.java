55f9a444cd4986e4263b83d8484b66c88e171758
Intrager <intrager@naver.com>
2021-10-21 21:01:48 +0900

    창업대전 ver 소스

--- DevelopTest/JAVATEST/src/main/java/passion/spring/env/controller/ProjectController.java
+++ DevelopTest/JAVATEST/src/main/java/passion/spring/env/controller/ProjectController.java
@@ -7,13 +7,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class ProjectController {
-
-    @GetMapping("/project-details")
-    public String projectDetails() {
-        return "project/project-details";
-    }
     @GetMapping("/projects")
     public String projects() {
         return "project/projects";
+    }
+    @GetMapping("/project-details")
+    public String projectdetails() {
+        return "project/project-details";
+    }
+    @GetMapping("/project-details2")
+    public String projectdetails2() {
+        return "project/project-details2";
+    }
+    @GetMapping("/project-details3")
+    public String projectdetails3() {
+        return "project/project-details3";
+    }
+    @GetMapping("/project-details4")
+    public String projectdetails4() {
+        return "project/project-details4";
+    }
+    @GetMapping("/project-details5")
+    public String projectdetails5() {
+        return "project/project-details5";
+    }
+    @GetMapping("/project-details6")
+    public String projectdetails6() {
+        return "project/project-details6";
+    }
+    @GetMapping("/project-details7")
+    public String projectdetails7() {
+        return "project/project-details7";
+    }
+    @GetMapping("/project-details8")
+    public String projectdetails8() {
+        return "project/project-details8";
     }
 }
